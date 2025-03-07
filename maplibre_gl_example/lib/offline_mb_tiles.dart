7f5b0b9736e69ee486eb406aad664433f5af75b2
Amjad islam <amjad@pinpointworks.com>
2025-03-07 20:27:22 +0500

    Tasks Done: -> Modify the maplibre_gl_example app main page as follows. -> Create a new example page named "Offline MBTiles -> This page should display the provided sample synthetic.mbtiles file from local storage

--- maplibre_gl_example/lib/offline_mb_tiles.dart
+++ maplibre_gl_example/lib/offline_mb_tiles.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
 import 'package:maplibre_gl_example/page.dart';
+import 'package:path_provider/path_provider.dart';
 
-class OfflineMBTilesPage extends ExamplePage {
+class OfflineMBTilesPage extends ExamplePage  {
   const OfflineMBTilesPage({super.key})
       : super(const Icon(Icons.map), 'Offline MBTiles');
 
@@ -21,63 +24,113 @@ class OfflineMBTiles extends StatefulWidget {
 
 class _OfflineMBTilesPageState extends State<OfflineMBTiles> {
   MaplibreMapController? mapController;
+  String mbtilesPath = "";
 
-  void _onMapCreated(MaplibreMapController controller) async {
-    mapController = controller;
-
-    await controller.addSource(
-      'mbtiles-source',
-      const RasterSourceProperties(
-        tiles: ['file:///assets/synthetic.mbtiles'],
-        tileSize: 256,
-      ),
-    );
-    await controller.addLayer(
-      'mbtiles-layer',
-      'mbtiles-source',
-      const RasterLayerProperties(
-        rasterOpacity: 1.0,
-      ),
-    );
+  @override
+  void initState()  {
+    super.initState();
 
   }
+
+  void _onMapCreated(MapLibreMapController controller) {
+    mapController = controller;
+  }
+
+  Future<void> loadMbtiles() async {
+    print("file path>>>>>>>>>>>>>>>>>> : $mbtilesPath");
+    const sourceId = 'MbtileSourceId';
+    // const layerId = 'MbtileLayerID';
+    if (!await File(mbtilesPath).exists()) {
+      print("file do not exist");
+    }else{
+      print("file  exist");
+
+    }
+    mapController!.addSource(
+      sourceId,
+      RasterSourceProperties(
+        tiles: ['mbtiles://$mbtilesPath'],
+        tileSize: 256,
+        attribution: 'Map data &copy; OpenStreetMap contributors',
+      ),
+    );
+    print('Source added: mbtiles-source');
+    mapController!.addLayer(
+      sourceId,
+      sourceId,
+      const RasterLayerProperties(),
+    );
+    print('Layer added: mbtiles-layer');
+    mapController!.moveCamera(
+      CameraUpdate.newLatLngZoom(const LatLng(30.7, -88.0), 6), // Mobile, AL
+    );
+    setState(() {});
+  }
+
+
+  Future<String> copyMbtilesFromAssets() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final filePath = '${directory.path}/map.mbtiles';
+    try {
+      if (!await File(filePath).exists()) {
+        final data = await rootBundle.load('assets/synthetic.mbtiles');
+        final bytes = data.buffer.asUint8List();
+
+        await File(filePath).writeAsBytes(bytes);
+      }
+    } catch (e) {
+      print(e);
+    }
+
+    return filePath;
+  }
+
+  Future<void> _loadMbtiles() async {
+    mbtilesPath = await copyMbtilesFromAssets();
+    loadMbtiles();
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [MaplibreMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: const CameraPosition(
-            target: LatLng(0.0, 0.0),
-            zoom: 1,
-          ),
-        ),
-          Positioned(
-            right: 10,
-            top: MediaQuery.of(context).size.height * 0.5 - 60,
-            child: Opacity(
-              opacity: 0.5,
-              child: Column(
-                children: [
-                  FloatingActionButton(
-                    onPressed: () => mapController?.animateCamera(
-                      CameraUpdate.zoomIn(),
-                    ),
-                    child: const Icon(Icons.zoom_in),
+      body:Stack(children: [
+              MaplibreMap(
+                  onMapCreated: _onMapCreated,
+                  onStyleLoadedCallback: (){
+                    _loadMbtiles();
+                  },
+                  initialCameraPosition: const CameraPosition(
+                    target: LatLng(0.0, 0.0),
+                    zoom: 1,
+                  )),
+
+              Positioned(
+                right: 10,
+                top: MediaQuery.of(context).size.height * 0.5 - 60,
+                child: Opacity(
+                  opacity: 0.5,
+                  child: Column(
+                    children: [
+                      FloatingActionButton(
+                        onPressed: () => mapController?.animateCamera(
+                          CameraUpdate.zoomIn(),
+                        ),
+                        child: const Icon(Icons.zoom_in),
+                      ),
+                      const SizedBox(height: 10),
+                      FloatingActionButton(
+                        onPressed: () => mapController?.animateCamera(
+                          CameraUpdate.zoomOut(),
+                        ),
+                        child: const Icon(Icons.zoom_out),
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 10),
-                  FloatingActionButton(
-                    onPressed: () => mapController?.animateCamera(
-                      CameraUpdate.zoomOut(),
-                    ),
-                    child: const Icon(Icons.zoom_out),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
-        ]
-      ),
+            ]),
     );
   }
 }
